2506be65ac1d76783512336cfe6d7be14a7e13d8
LukacTomas <lukactv@gmail.com>
2021-11-03 21:16:23 +0100

    manipulate_image.py updated for saving images
    With function save_image  we can save image object to destination

--- Week1/manipulate_image.py
+++ Week1/manipulate_image.py
@@ -1,17 +1,31 @@
 from PIL import Image
 import os
 import os.path
+from datetime import datetime
 
-images = ["code1.png", "code2.png"]
+
+def get_images_from_dir(dir='.', allowed_types=[".png", ".jpg", ".jpeg", ".bmp", ".webp"]):
+    """ Function for getting all images from current directory
+    Args:
+        dir (string): Set the working directory.
+        allowed_types (list)): List of allowed image types -[".png", ".jpg", ".jpeg", ".bmp", ".webp"] .
+    """
+    images = []
+    for file in os.listdir(dir):
+        extension = os.path.splitext(file)[1]
+        if extension.lower() not in allowed_types:
+            continue
+        images.append(file)
+    return images
 
 
-def resize_image(image, rezize_size=(128, 128)):
+def resize_image(image, resize_size=(128, 128)):
     """ Function resize_image for resizing `image` object
     Args:
         image (object): Image object from PIL.
         rezize_size (Tupple)): Resize size = default is (128, 128).
-     """
-    return image.rezize(rezize_size)
+    """
+    return image.resize(resize_size)
 
 
 def rotate_image(image, degrees=0):
@@ -19,25 +33,33 @@ def rotate_image(image, degrees=0):
     Args:
         image (object): Image object from PIL.
         degrees (number)): Degrees of rotation = default is 0.
-     """
+    """
     return image.rotate(degrees)
 
 
-def get_images_from_dir(dir='.', allowed_types=[".png", ".jpg", ".jpeg", ".bmp", ".webp"]):
-    """ Function for getting all images from current directory
-    Args:
-        dir (string): Set the working directory.
-        allowed_types (list)): List of allowed image types -["png", "jpg", "jpeg", "bmp", "webp"] .
+def save_image(image, destination=''):
+    """Simple function for saving image object to destination
+       Args:
+       image (object): Image object from PIL.
+       destination (string): String that represents dir and filename
     """
-    images = []
-    for file in os.listdir(dir):
-        extension = os.path.splitext(file)[1]
-        print(extension, file, extension in allowed_types)
-        if extension.lower() not in allowed_types:
-            continue
-        images.append(file)
-    return images
+    # check if destination already exists
+    # if exist add timestamp to destination name
+    if destination == "":
+        destination = str(datetime.now()) + str(image.format)
+
+    if os.path.isfile(destination):
+        timestamp = int(datetime.now().timestamp())
+        dest_array = os.path.splitext(destination)
+        destination = dest_array[0] + str(timestamp) + dest_array[1]
+    image.save(destination)
 
 
 if __name__ == "__main__":
-    pass
+    images = get_images_from_dir()
+    for image in images:
+        extension = os.path.splitext(image)[1]
+        with Image.open(image) as im:
+            im = rotate_image(im, 90)
+            im = resize_image(im)
+            save_image(im, image)
